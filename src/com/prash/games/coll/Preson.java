e53e7128511fdb7b1fc848820d7583019fcdc46a
Prashanth Rajendra <Prashanth.Rajendra@nokia.com>
2020-04-27 07:22:44 +0530

    test

---
+++ src/com/prash/games/coll/Preson.java
@@ -0,0 +1,32 @@
+package com.prash.games.coll;
+
+public class Preson {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		String str1 =  new String("abc");
+		String str2 = new String("abc");
+		
+		
+		
+		
+		// str1 =  str2;
+		
+		
+		
+		
+		if(str1 == str2 ) {
+			System.out.println("str1 == str2");
+		}
+		if(str1.equals(str2)) {
+			System.out.println("str1.equals(str2)");
+		}
+
+	}
+
+}
